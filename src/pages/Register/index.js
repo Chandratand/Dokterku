b1d3181d0083f4a5049c36f2483cb580efface40
chandratand <Chandra.tand@gmail.com>
2021-10-28 16:16:04 +0700

    menyimpan data user ke firebase database

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -23,6 +23,15 @@ const Register = ({navigation}) => {
       .then(success => {
         setLoading(false);
         setForm('reset');
+        // https://firebse.com/users/
+        const data = {
+          fullName: form.fullName,
+          profession: form.profession,
+          email: form.email,
+        };
+        Fire.database()
+          .ref('users/' + success.user.uid + '/')
+          .set(data);
         console.log('register success: ', success);
       })
       .catch(error => {
