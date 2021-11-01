0a96b1ad19f5cba38089a50f1cb123debab9bc0e
chandratand <Chandra.tand@gmail.com>
2021-11-01 21:39:21 +0700

    implementasikan Redux untuk memunculkan Loading secara global

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,10 +5,16 @@ import {createStore} from 'redux';
 //setprofile("Natasha")
 
 const initialState = {
-  loadding: false,
+  loading: false,
 };
 
 const reducer = (state = initialState, action) => {
+  if (action.type === 'SET_LOADING') {
+    return {
+      ...state,
+      loading: action.value,
+    };
+  }
   return state;
 };
 
