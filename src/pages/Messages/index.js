43dec509caf3522a22d783d0b6e6badaf320c556
chandratand <Chandra.tand@gmail.com>
2021-11-05 16:16:15 +0700

    memunculkan fullName dan Photo di halaman messages (History Messages)  menggunakan Join

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -6,49 +6,39 @@ import {colors, fonts, getData} from '../../utils';
 import {Fire} from '../../config';
 
 const Messages = ({navigation}) => {
-  const [doctors] = useState([
-    {
-      id: 1,
-      profile: DummyDoctor4,
-      name: 'Alexander Jannie',
-      desc: 'Baik ibu, terima kasih banyak atas wakt...',
-    },
-    {
-      id: 2,
-      profile: DummyDoctor5,
-      name: 'Nairobi Putri Hayza',
-      desc: 'Oh tentu saja tidak karena jeruk it...',
-    },
-    {
-      id: 3,
-      profile: DummyDoctor6,
-      name: 'John McParker Steve',
-      desc: 'Oke menurut pak dokter bagaimana unt...',
-    },
-  ]);
   const [user, setUser] = useState({});
   const [historyChat, setHistoryChat] = useState([]);
 
   useEffect(() => {
     getDataUserFromLocal();
+    const rootDB = Fire.database().ref();
     const urlHistory = `messages/${user.uid}/`;
-    Fire.database()
-      .ref(urlHistory)
-      .on('value', snapShot => {
-        console.log('data History', snapShot.val());
-        if (snapShot.val()) {
-          const oldData = snapShot.val();
-          const data = [];
-          Object.keys(oldData).map(key => {
-            data.push({
-              id: key,
-              ...oldData[key],
-            });
+    const messagesDB = rootDB.child(urlHistory);
+
+    messagesDB.on('value', async snapShot => {
+      console.log('data History', snapShot.val());
+      if (snapShot.val()) {
+        const oldData = snapShot.val();
+        const data = [];
+
+        const promises = await Object.keys(oldData).map(async key => {
+          const urlUidDoctor = `doctors/${oldData[key].uidPartner}`;
+          const detailDoctor = await rootDB.child(urlUidDoctor).once('value');
+          console.log('detail doctor', detailDoctor.val());
+
+          data.push({
+            id: key,
+            detailDoctor: detailDoctor.val(),
+            ...oldData[key],
           });
-          console.log('new Data History : ', data);
-          setHistoryChat(data);
-        }
-      });
+        });
+
+        await Promise.all(promises);
+
+        console.log('new Data History : ', data);
+        setHistoryChat(data);
+      }
+    });
   }, [user.uid]);
 
   const getDataUserFromLocal = () => {
@@ -63,8 +53,8 @@ const Messages = ({navigation}) => {
         return (
           <List
             key={chat.id}
-            profile={chat.uidPartner}
-            name={chat.uidPartner}
+            profile={{uri: chat.detailDoctor.photo}}
+            name={chat.detailDoctor.fullName}
             desc={chat.lastContentChat}
             onPress={() => navigation.navigate('Chatting')}
           />
